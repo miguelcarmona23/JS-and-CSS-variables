a12974150df33e05dd1ed42f7488a482759e3c17
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-01-30 17:32:24 -0500

    added input addeventlistener for change

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,5 @@ const inputs = document.querySelectorAll('.controls input');
 function handleUpdate() {
     console.log(this.value);
 }
+
+inputs.forEach(input => input.addEventListener('change',handleUpdate));
